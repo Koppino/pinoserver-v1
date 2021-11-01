21e6941785f90b5b2dce416e340e00b1931f7bf3
Koppino <kopp.radek@email.cz>
2021-11-01 12:51:46 +0100

    101

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,9 +1,13 @@
+const Person = require("../models/Person");
 const Zaznam = require("../models/Zaznam");
 
 
 module.exports.getAccountView = (req, res) => {
     Zaznam.find({user:req.user},null,{sort:{createdAt:1}}, (err,posts) => {
         if(err)console.log(err);
-        res.render('acc',{user: req.user, posts:posts})
+        Person.find({user:req.user}, (err, people) => {
+            if(err) console.log(err);
+            res.render('acc',{user: req.user, posts:posts, people:people})
+        })
     })
 };
