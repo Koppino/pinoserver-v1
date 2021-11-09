a35d76038ae70dfe0f873815a606e8eac1776830
Koppino <kopp.radek@email.cz>
2021-11-09 02:11:52 +0100

    1001

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,6 +1,7 @@
 const Person = require("../models/Person");
 const Zaznam = require("../models/Zaznam");
-
+const bcrypt = require('bcryptjs');
+const User = require("../models/User");
 
 module.exports.getAccountView = (req, res) => {
     Zaznam.find({user:req.user},null,{sort:{createdAt:1}}, (err,posts) => {
@@ -18,5 +19,29 @@ module.exports.getChangePaswword = (req, res) => {
 }
 
 module.exports.doChangePaswword = (req, res) => {
+    const _id = req.user._id
+    const newpassword = req.body.password
+    const newpassword2 = req.body.password2
 
+    if (newpassword != newpassword2) {
+        req.flash('danger', 'Hesla musí být stejné.')
+        res.render('acc', {user:req.user, message: req.flash('danger')})
+    }
+
+    User.findOne({_id: _id }, (err, iam) => {
+        if(err) console.log(err);
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newpassword, salt, (err, hash) => {
+              if (err) throw err;
+              iam.password = hash;
+              iam
+                .save()
+                .then((user) => {
+                 res.redirect('/acc')
+                })
+                .catch((err) => console.log(err));
+            });
+          });
+    })
 }
--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,3 +1,4 @@
+const Person = require("../models/Person");
 const User = require("../models/User");
 const Zaznam = require('../models/Zaznam')
 module.exports.getHomepage = (req, res) => {
@@ -6,11 +7,38 @@ module.exports.getHomepage = (req, res) => {
 };
 
 
-module.exports.getDashboard = (req, res) => {
-    Zaznam.find({personName: req.user.username},null,{sort:{createdAt: -1}}, (err, posts) => {
+module.exports.getDashboardd = (req, res) => {
+    let tydenZpet = [];
+    console.log(tydenZpet.length)
+    Zaznam.find({personName: req.user.username},null,{sort:{createdAt: -1}}, async (err, posts) => {
         if(err)console.log(err)
         if(!posts) res.render('dashboard', {posts: null, user:req.user})
+        const predTydnem = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+        const predDvoumaDnama = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000)
+        console.log(predTydnem.getTime())
+        console.log(predDvoumaDnama.getTime())
 
-        res.render('dashboard', {user : req.user, posts:posts, message: req.flash('danger')})
+        await posts.forEach(post => {
+            if(post.createdAt.getTime() >= predTydnem.getTime()) {
+                console.log(post.createdAt.getTime() + " " + post.createdAt)
+
+                tydenZpet.push(post)
+                console.log(tydenZpet.length)
+                console.log(tydenZpet)
+            }else { console.log("starsi")
+        }} )
+        res.render('dashboard', {user : req.user, posts:posts,postsTydenZpet:tydenZpet, message: req.flash('danger')})
     })
+}
+
+module.exports.getDashboard = (req, res) => {
+    if(req.user.admin) {
+        res.render('dashboard', {user:req.user})
+    }else {
+        Person.findOne({personName: req.user.username}, (err, iam) => {
+            if(err) throw(err);
+            const kokot = `/person/get/${iam._id}`
+            res.redirect(kokot)
+        })
+    }
 }
